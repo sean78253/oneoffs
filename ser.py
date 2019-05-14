bb41ec4ff24439be6f5919747dc73099a20c20b1
sean78253 <feb09@usersunion.net>
2019-05-14 17:22:06 -0500

    General updates adding comments

--- ser.py
+++ ser.py
@@ -1,5 +1,9 @@
 # refer to https://www.google.com/search?client=firefox-b-1-d&q=python+define+and+return+variable
-# To do the serial gram in bash - x=`grep ^Serial /proc/cpuinfo | awk '{print $3}'`
+# To do the serial number in bash - x=`grep ^Serial /proc/cpuinfo | awk '{print $3}'`
+# General use case is a python script RESTful call where you need to know which Pi is talking to you.
+# Sure, it can be hand set to whatever serial number a evil person wants to set, but in my use case there
+# are other unrelated to the PI itself that have unique serial numbers, so I'll know if someone is farting around. 
+
 def getserial():
   # Extract serial from cpuinfo file
   cpuserial = "0000000000000000"
