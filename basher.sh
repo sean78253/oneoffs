83584add3aa07a6a95db9012f237b44ba8f0b709
sean78253 <sean_e6@yahoo.com>
2021-12-07 18:47:47 -0600

    formatting fix

--- basher.sh
+++ basher.sh
@@ -44,6 +44,7 @@ if [ -z "$SSH_AUTH_SOCK" ] ; then
 	ssh_keys=`find ~/.ssh/id_rsa* | grep -v '\.pub' | xargs` && ssh-add -q ${ssh_keys}
 	unset ssh_keys
 fi
+
 function cleanup()
 {
 	test -n "$SSH_AGENT_PID" && eval `/usr/bin/ssh-agent -k`
